c7f6464bdb41b5706b573bad25953f8750a095cc
Dennis de Jesus <ddejesus@outlook.com>
2024-10-30 15:00:52 -0400

    SharePoint Sync PowerShell Scripts

---
+++ SharePoint-LibrarySync/GetSharePoint-SiteID-WebID-ListID.ps1
@@ -0,0 +1,34 @@
+﻿#Want to give credit to where I got the info from. I put them together into one script. Links below. 
+#Read more: https://www.sharepointdiary.com/2018/08/get-list-id-sharepoint-online-powershell.html#ixzz8kP4MFV5v
+#Read more: https://www.sharepointdiary.com/2018/04/sharepoint-online-powershell-to-get-site-collection-web-id.html#ixzz8kOye4ywE
+
+#Parameter
+$SiteURL = "SiteURL EX: https://SiteName.sharepoint.com/sites/site"
+$ListName = "ListName"
+ 
+#Get Credentials to connect
+$Cred= Get-Credential
+  
+#Setup the context
+$Ctx = New-Object Microsoft.SharePoint.Client.ClientContext($SiteURL)
+$Ctx.Credentials = New-Object Microsoft.SharePoint.Client.SharePointOnlineCredentials($Cred.Username, $Cred.Password)
+ 
+#Get Site and Web Objects
+$Ctx.Load($Ctx.Site)
+$Ctx.Load($Ctx.Web)
+$Ctx.ExecuteQuery()
+
+#Get the List
+$List = $Ctx.Web.Lists.GetByTitle($ListName)
+$Ctx.Load($List)
+$Ctx.ExecuteQuery()
+
+ 
+#sharepoint online powershell get site collection id
+Write-host -f Green "Site ID:"$Ctx.Site.Id
+ 
+#sharepoint online powershell get site id
+Write-host -f Green "Web ID:"$Ctx.Web.Id
+
+#Get List GUID
+Write-host -f Green "List ID:"$List.Id
---
+++ SharePoint-LibrarySync/Sync-SP-LibraryFinal.ps1
@@ -0,0 +1,34 @@
+﻿# Define the local path where the SharePoint library is synced
+$localPath = "$env:USERPROFILE\SharePoint Site Name\WebTitle + ListTitle goes here"
+
+# Check if the local path exists
+if (Test-Path -Path $localPath) {
+    Write-Output "The document library is already synced."
+    exit
+} else {
+    Write-Output "The document library is not synced. Proceeding with sync..."
+    # Your existing sync logic goes here
+    # Example: Start-SyncLibrary -Path $localPath -SharePointUrl "https://yoursharepointsite.com/library"
+}
+
+# Your existing script continues here
+
+Add-Type -AssemblyName System.Web
+$WebURL = [System.Web.HttpUtility]::UrlEncode("WebURL")
+$SiteID = [System.Web.HttpUtility]::UrlEncode("SiteID")
+$WebID = [System.Web.HttpUtility]::UrlEncode("WebID")
+$ListID = [System.Web.HttpUtility]::UrlEncode("ListID")
+$WebTitle = [System.Web.HttpUtility]::UrlEncode("WebTitle")
+$ListTitle = [System.Web.HttpUtility]::UrlEncode("ListTitle")
+#$ListTitle = "ListTitle if it has spaces in the name" -replace " ", "%20"  # Manually replace spaces with %20
+
+# Give Windows some time to load before getting the email address
+Start-Sleep -s 20
+
+$UserName = $env:USERNAME
+$Domain = "Domain Name"
+
+Write-Output "Configure OneDrive"
+$odopen = "odopen://sync/?onPrem=0&webTemplate=68&libraryType=4&siteId=" + $SiteID + "&webId=" + $WebID + "&webUrl=" + $webURL + "&listId=" + $ListID + "&userEmail=" + $UserName + $Domain + "&webTitle=" + $WebTitle + "&ListTitle=" + $ListTitle
+
+Start-Process $odopen
